30f29497a49c25c3e193f8d1a847978aafb4ce01
Lino <robertolino2386@gmail.com>
2026-08-06 21:54:44 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-// URL do Aplicativo da Web do Google Apps Script (Etapa 2)
-const API_URL = 'https://script.google.com/macros/s/AKfycbwrSoIa0c0L1sIExbuLXLnusnXzx5szODAItwhT3IXNQqWGqdcMMBILe5kWuefdp8F9Mg/exec';
-
-const SHEET_KEYS = ['Lancamentos', 'Parcelas', 'ClientesFornecedores', 'Categorias', 'ContasBancarias', 'CartoesCredito'];
